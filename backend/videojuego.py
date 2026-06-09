9d850ffb25e57395bee15c5c57afd7c635374169
bryanernesto1999 <bryan.sabogalc@istpargentina.edu.pe>
2026-06-09 14:11:12 -0500

    se agregaron la clase padre productos y la clase hijos y se definieron los metodos

--- backend/videojuego.py
+++ backend/videojuego.py
@@ -1,9 +1,47 @@
 class Producto:
-    """Clase base que representa un producto genérico."""
- 
-    def __init__(self, id: int, nombre: str, precio: float, stock: int):
-        """Inicializa un producto con sus atributos básicos."""
+    def __init__(self, id, nombre, precio, stock):
         self.__id = id
         self.__nombre = nombre
         self.__precio = precio
         self.__stock = stock
+    
+    def get_id(self):
+        return self.__id
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_precio(self):
+        return self.__precio
+
+    def get_stock(self):
+        return self.__stock
+
+    def set_stock(self, stock):
+        self.__stock = stock
+
+    def mostrar_info(self):
+        return f"{self.__nombre} - S/.{self.__precio} - Stock: {self.__stock}"    
+    
+class Videojuego(Producto):
+    def __init__(self, id, nombre, precio, stock,
+                 genero, plataforma):
+        super().__init__(id, nombre, precio, stock)
+
+        self.__genero = genero
+        self.__plataforma = plataforma
+
+    def get_genero(self):
+        return self.__genero
+
+    def get_plataforma(self):
+        return self.__plataforma
+
+    def mostrar_info(self):
+        return (
+            f"Videojuego: {self.get_nombre()} | "
+            f"Género: {self.__genero} | "
+            f"Plataforma: {self.__plataforma}"
+        )
+        
+        
